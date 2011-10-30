ba0b49c01775c876384674798254101e7afbf585
Christiane Ruetten <cr@23bit.net>
2011-10-30 20:58:09 +0100

    pins includes files added

---
+++ aDMX/pins_v1.h
@@ -0,0 +1,19 @@
+#ifndef __PINS_V1_H_
+#define __PINS_V1_H_
+
+// Pins for aDMX shield v1
+
+#define RX_IN 7
+#define TX_OUT 8
+#define DRV_ENA 9
+#define RCV_ENA 0
+#define ROTARY_A 5
+#define ROTARY_B 6
+#define BUTTON_A 4
+#define BUTTON_B 3
+#define BUTTON_R 2
+#define LED_A 10
+#define LED_B 11
+
+#endif //__PINS_V1_H_
+
---
+++ aDMX/pins_v2.h
@@ -0,0 +1,19 @@
+#ifndef __PINS_V2_H_
+#define __PINS_V2_H_
+
+// Pins for aDMX Shield v2
+
+#define RX_IN 7
+#define TX_OUT 8
+#define DRV_ENA 9
+#define RCV_ENA 0
+#define ROTARY_A 3
+#define ROTARY_B 4
+#define BUTTON_A A2
+#define BUTTON_B A3
+#define BUTTON_R A4
+#define LED_A 5
+#define LED_B 6
+
+#endif //__PINS_V2_H_
+
